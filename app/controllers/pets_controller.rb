24b306ae5b4c1be792431e2998024d1204aab0f6
An Nuyen <annuyen@gmail.com>
2018-02-28 04:34:05 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,16 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    @pet.owner_id = params[:pet][:owner_id]
+    if !params[:owner_name].empty? 
+      @pet.owner = Owner.create(name: params[:owner_name])
+    end 
     redirect to "pets/#{@pet.id}"
   end
 
